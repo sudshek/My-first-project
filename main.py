6ebf8931bac5836415f56b0043560f44c54d5fa0
sudshek <singhsudhanshu204@gmail.com>
2017-03-20 16:02:05 +0530

    ADDED MAIN FILE WHICH RUNS THE PROGRAM ENTERED BY USER

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#sudhasnhu
+var  = raw_input("Which file do you want to open")
+if var == "student_becomes_teacher.py":
+    import student_becomes_teacher
+elif var == "battle_ship.py":
+    import battle_ship
+elif var == "exam-stastistics":
+    import exam_stastistics
+else :
+    print "Wrong input!! No such file exists!!"
